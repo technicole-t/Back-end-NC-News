0bb9ec10075b3e21c0db0e0a5fae728fb4566c10
technicole-t <thorleynicole@gmail.com>
2020-03-18 09:02:20 +0000

    first test written for utility functions and passing

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,26 @@
-exports.formatDates = list => {};
+// You should employ full TDD as you build these functions.
+// If you need extra guidance refer back to your schema in the migrations
+// files and look at how the data provided differs from the structure set out in the schema.
 
+// This utility function should be able to take an array (list) of objects
+// and return a new array. Each item in the new array must have its timestamp converted
+// into a Javascript date object. Everything else in each item must be maintained.
+exports.formatDates = list => {
+  if (list.length === 0) return [];
+};
+
+// This utility function should be able to take an array (list) of objects and return
+// a reference object. The reference object must be keyed by each item's title, with the
+// values being each item's corresponding id. e.g.
+// [{ article_id: 1, title: 'A' }] becomes { A: 1 }
 exports.makeRefObj = list => {};
 
+// This utility function should be able to take an array of comment objects (comments)
+// and a reference object, and return a new array of formatted comments.
+// Each formatted comment must have:
+// Its created_by property renamed to an author key
+// Its belongs_to property renamed to an article_id key
+// The value of the new article_id key must be the id corresponding to the original title value provided
+// Its created_at value converted into a javascript date object
+// The rest of the comment's properties must be maintained
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,23 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils.js");
 
-describe('formatDates', () => {});
+// empty array
+// array of one obj
+// array of multiple obj
+//check for mutations
+describe("formatDates", () => {
+  it("returns an empty array when passed an empty array", () => {
+    const list = [];
+    const actual = formatDates(list);
+    const expected = [];
+    expect(actual).to.eql(expected);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
